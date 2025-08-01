6413b4c9f197af1a5273df39a50345f76fab7bc5
Testing_umadevi <mail2umadevi.a@gmail.com>
2025-08-01 20:19:41 +0530

    committed.

---
+++ Umadevi/practice_code/first_code.py
@@ -0,0 +1,82 @@
+x = 10
+print(x)
+
+# multiple value is same also address will same
+
+p=50
+q=50
+print("p:", p)
+#addition of two numbers
+a=5
+b=5
+print(a+b)
+x=y=z=100+6*2 # 100=100=100 6+6+6 2*2*2
+print (x+y+z)
+
+#subtraction
+x = 80
+v = 20
+
+print("Sub :", x - v)
+print("Sub :", x * v)
+print("Sub :", x % v)
+print("Sub :", x / v)
+
+name="umadevi"
+p=6
+print("Result: ", name*p)
+
+
+#take three numbers /3
+a=6
+b=8
+c=9
+print("value:",a+b+c/3)
+
+#Python Basic Programs, Exercises
+
+#Python Program to add two integer values.
+
+a=3
+b=6
+print("value:", a+b)
+
+#Python Program to subtract two integer values.
+
+a=19
+b=10
+print("value:", a-b)
+
+#Python program to multiply two numbers.
+
+u =120
+me=100
+print("u and me is:", u*me)
+
+#Python program to repeat a given string 5 times.
+uam ="devi"
+n1= 10
+
+print("join:" ,uam*n1 )
+
+ #Python program to get the Average of given numbers.
+#Formula: sum of all the number/ total number
+#Input:
+a = 40
+b = 50
+c = 30
+print("what is it:" , a+b+c/3)
+
+ #Python program to print the square and cube of a given number.
+#Input :
+num1 = 9
+
+Square = 81
+
+
+print("value : ",num1**2)
+
+
+
+
+
