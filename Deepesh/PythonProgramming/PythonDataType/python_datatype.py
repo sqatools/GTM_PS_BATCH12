b320102d2e25aaab4440e134aa766ea95fe2c906
Deepesh <xovek11875@bitvoo.com>
2025-08-04 21:07:35 +0530

    data type session2

--- Deepesh/PythonProgramming/PythonDataType/python_datatype.py
+++ Deepesh/PythonProgramming/PythonDataType/python_datatype.py
@@ -107,3 +107,125 @@ print(add_complex_number)
 
 print("_" * 50)
 ############ String ############
+"""
+# Properties:
+->  string is immutable data type.
+->  we can define string with single/double/triple quotes.
+->  string follows positive and negative indexing.
+->  string can contains any type of data as raw string.
+"""
+
+s1 = ''
+s2 = "Hello good morning"
+s3 = """
+Thank you, India and England. This was a Test series of the highest order. 
+This was the Ali vs Foreman of cricket - a battle where two heavyweights 
+went blow for blow to make the whole world rumble with excitement. 
+A battle where after 25 days of hard, grueling cricket we still had 
+no outright victors. Just two champion teams in their own right who 
+have given the red ball some reverence once again in this age of T20 extravagance.
+What can we say but thank you
+"""
+
+print(s1, type(s1))  # <class 'str'>
+
+print("_"*50)
+print(s2, type(s2))
+# Hello good morning <class 'str'>
+
+print("_"*50)
+print(s3, type(s3))
+"""
+Thank you, India and England. This was a Test series of the highest order. 
+This was the Ali vs Foreman of cricket - a battle where two heavyweights 
+went blow for blow to make the whole world rumble with excitement. 
+A battle where after 25 days of hard, grueling cricket we still had 
+no outright victors. Just two champion teams in their own right who 
+have given the red ball some reverence once again in this age of T20 extravagance.
+What can we say but thank you
+ <class 'str'>
+"""
+
+# string follows the indexing
+str4 = "Hello"
+
+"""
+ 0  1   2   3   4  +ve
+ H  e   l   l   o
+-5 -4  -3  -2  -1  -ve
+"""
+
+print(str4[0])   # H +ve
+print(str4[-5])  # H -ve
+
+print(str4[3], str4[-2]) # l l
+
+print("_" * 50)
+################## List #################
+"""
+Properties:
+->  List is mutable data type, we can modify/update list values at any point of time.
+->  List contains values in square bracket. e.g  [6, 8, 3]
+->  List can contain all types of data, int, float, complex, string, list, tuple, dictionary, set, boolean
+->  List also follows +ve and -ve indexing as like string.
+->  Generally we use list data type, where we have dynamic data, e.g. 
+    student registration, 
+    employee management.
+"""
+
+list1 = [3, 4.5, 5+7j,  'Hello', [7, 8, 3], (2, 5, 8), {'a': 123}, {4, 6, 7}, True]
+
+print(list1, type(list1))
+# [3, 4.5, (5+7j), 'Hello', [7, 8, 3], (2, 5, 8), {'a': 123}, {4, 6, 7}, True] <class 'list'>
+
+
+list2 = [5, 7, 9, 3]
+
+"""
+ 0   1    2      3   +ve
+[5,  7,   9,     3]
+-4   -3   -2    -1   -ve
+"""
+
+print(list2[1])  # 7
+print(list2[-3]) # 7
+
+
+list3 = [6, 7, 9]
+list3.append(100)
+print(list3, type(list3))
+# [6, 7, 9, 100] <class 'list'>
+list3.insert(0, 700)
+print(list3, type(list3))
+# [700, 6, 7, 9, 100] <class 'list'>
+
+
+
+
+print("_" * 50)
+################## Tuple #################
+"""
+Properties:
+->  Tuple is immutable data type, we can not modify/update Tuple values.
+->  Tuple contains values in square bracket. e.g  (6, 8, 3)
+->  Tuple can contain all types of data, int, float, complex, string, list, tuple, dictionary, set, boolean
+->  Tuple also follows +ve and -ve indexing as like string and list
+->  Generally we use Tuple data type, where we have fixed data, \
+    e.g. Months in year, days in week. 
+"""
+
+tup1 = (3, 6, 8, 1)
+print(tup1, type(tup1))
+# (3, 6, 8, 1) <class 'tuple'>
+
+tup2 = (3, 5.6, 'Python', [4, 6, 7], (1, 6, 2), {'a': 12}, {6, 7, 2}, True, False, None)
+print(tup2, type(tup2))
+
+# (3, 5.6, 'Python', [4, 6, 7], (1, 6, 2), {'a': 12}, {2, 6, 7}, True, False, None) <class 'tuple'>
+
+tup3 = (5, 7, 99)
+print(tup3[2])  # 99
+print(tup3[-2]) # 7
+
+print(tup2[4])  # (1, 6, 2)
+print(tup2[-4]) # {2, 6, 7}
