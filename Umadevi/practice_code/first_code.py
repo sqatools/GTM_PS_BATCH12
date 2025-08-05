3c625a213bc7974da4a4e441fca62c6786f33172
Testing_umadevi <mail2umadevi.a@gmail.com>
2025-08-05 19:28:50 +0530

    committed.

--- Umadevi/practice_code/first_code.py
+++ Umadevi/practice_code/first_code.py
@@ -35,46 +35,87 @@ print("value:",a+b+c/3)
 
 #Python Basic Programs, Exercises
 
-#Python Program to add two integer values.
+#1.Python Program to add two integer values.
 
 a=3
 b=6
 print("value:", a+b)
 
-#Python Program to subtract two integer values.
+#2.Python Program to subtract two integer values.
 
 a=19
 b=10
 print("value:", a-b)
 
-#Python program to multiply two numbers.
+#3.Python program to multiply two numbers.
 
 u =120
 me=100
 print("u and me is:", u*me)
 
-#Python program to repeat a given string 5 times.
+#4.Python program to repeat a given string 5 times.
 uam ="devi"
 n1= 10
 
 print("join:" ,uam*n1 )
 
- #Python program to get the Average of given numbers.
+ #5.Python program to get the Average of given numbers.
 #Formula: sum of all the number/ total number
 #Input:
-a = 40
-b = 50
-c = 30
-print("what is it:" , a+b+c/3)
+a = 20
+b = 30
+c = 40
+print("what is it:" ,(a+b+c)/3) #sum of all number # (20+30+40) then /3
 
- #Python program to print the square and cube of a given number.
+ #6.Python program to print the square and cube of a given number.
 #Input :
 num1 = 9
-
 Square = 81
+print("value : ",num1**2) #value :  81
+
+#Python program to print the square and cube of a given number.
+#Input :
+num1 = 9
+#Output :
+#Square = 81
+#Cube =   729
+
+print("sq:",(9**2)) # sq: 81
+print("cub:",(9**3)) #sq: 729
+# try to this equation
+# (a+b)^2 = a^2 + b^2 + 2ab
+
+#a = 5
+#b = 6
+#LHS = (a+b)**2
+#RHS = a**2 + b**2 + 2*a*b
+#print(LHS == RHS) # True
+
+#Python program to solve this Pythagorous theorem.
+#Theorem : (a2 + b2 = c2)
+
+#a= 10
+#b=3
+#c=2
+#LHS = (a**2)+(b**2)
+#RHS = (c**2) # with c is false withou c value  is true (10**2)+(3**2)=(2**2)=
+#print(LHS == RHS )
+#print(10**2)+(3**2) = (2**2)
 
 
-print("value : ",num1**2)
+#Python program to solve the given math formula.
+#Formula : (a + b)2 = a^2 + b^2 + 2ab
+
+a=8
+b=3
+LHS = (2+3)**2  # 5**2 = 25
+RHS = (2**2)+(3**2)+2*2*3  # 4 + 9 +12 =25
+print(LHS == RHS)
+print(LHS)
+print(RHS)
+
+
+
 
 
 
