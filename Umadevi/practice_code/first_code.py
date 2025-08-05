7de9c6e42b387b1bb47704ade84754e711a35d75
Testing_umadevi <mail2umadevi.a@gmail.com>
2025-08-05 20:25:42 +0530

    5/08/25

--- Umadevi/practice_code/first_code.py
+++ Umadevi/practice_code/first_code.py
@@ -73,7 +73,7 @@ num1 = 9
 Square = 81
 print("value : ",num1**2) #value :  81
 
-#Python program to print the square and cube of a given number.
+#7 Python program to print the square and cube of a given number.
 #Input :
 num1 = 9
 #Output :
@@ -91,7 +91,7 @@ print("cub:",(9**3)) #sq: 729
 #RHS = a**2 + b**2 + 2*a*b
 #print(LHS == RHS) # True
 
-#Python program to solve this Pythagorous theorem.
+#8.Python program to solve this Pythagorous theorem.
 #Theorem : (a2 + b2 = c2)
 
 #a= 10
@@ -103,7 +103,7 @@ print("cub:",(9**3)) #sq: 729
 #print(10**2)+(3**2) = (2**2)
 
 
-#Python program to solve the given math formula.
+#9.Python program to solve the given math formula.
 #Formula : (a + b)2 = a^2 + b^2 + 2ab
 
 a=8
@@ -113,6 +113,42 @@ RHS = (2**2)+(3**2)+2*2*3  # 4 + 9 +12 =25
 print(LHS == RHS)
 print(LHS)
 print(RHS)
+
+# 1/08/2025 --pending task
+# 4/08/2025
+# string
+
+a = ""
+b ="hi uma"
+c = """#indexing in string indicates the position of the index [0] for positive index starts and negative is [-1] """
+print(c,type(c))
+print(a,type(a))
+
+#indexing in string indicates the position of the index [0] for positive index starts and negative is [-1]
+
+value="uma"
+#U M A devi
+#0 1 2 3 4 5 6
+#-1 -2 -3
+
+print(value[0],value[1],value[2])  #uma need space one question
+#value.append(value)
+value.insert(0,1)
+print (value[-1],value[-2],value[-2])  #umm
+#List- we can change its mutable data type, we can def in [],() , all dat atype can use , inside the list of list we can use -ve , +ve also can follows
+
+a =[6,8,9]
+# one = [a+5j] #complex
+print(a) #this is not working
+print(a,type[a])
+
+5/08/2025
+
+
+
+
+
+
 
 
 
