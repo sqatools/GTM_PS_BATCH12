3520a0299900dfcea5488b7588b3d5238414dbf0
Testing_umadevi <mail2umadevi.a@gmail.com>
2025-08-10 18:22:52 +0530

    latest prcatice uploaded.

--- Umadevi/practice_code/first_code.py
+++ Umadevi/practice_code/first_code.py
@@ -1,4 +1,4 @@
-x = 10
+"""x = 10
 print(x)
 
 # multiple value is same also address will same
@@ -106,34 +106,32 @@ print("cub:",(9**3)) #sq: 729
 #9.Python program to solve the given math formula.
 #Formula : (a + b)2 = a^2 + b^2 + 2ab
 
-a=8
+"""
+"""a=8
 b=3
 LHS = (2+3)**2  # 5**2 = 25
 RHS = (2**2)+(3**2)+2*2*3  # 4 + 9 +12 =25
 print(LHS == RHS)
 print(LHS)
 print(RHS)
-
-# 1/08/2025 --pending task
-# 4/08/2025
 # string
-
 a = ""
 b ="hi uma"
 c = """#indexing in string indicates the position of the index [0] for positive index starts and negative is [-1] """
-print(c,type(c))
+"""print(c,type(c))
 print(a,type(a))
+"""
 
 #indexing in string indicates the position of the index [0] for positive index starts and negative is [-1]
 
-value="uma"
+"""value="uma"
 #U M A devi
 #0 1 2 3 4 5 6
 #-1 -2 -3
 
-print(value[0],value[1],value[2])  #uma need space one question
+print(value[0],value[1],value[2])
 #value.append(value)
-value.insert(0,1)
+#value.insert(0,1)
 print (value[-1],value[-2],value[-2])  #umm
 #List- we can change its mutable data type, we can def in [],() , all dat atype can use , inside the list of list we can use -ve , +ve also can follows
 
@@ -142,7 +140,86 @@ a =[6,8,9]
 print(a) #this is not working
 print(a,type[a])
 
-5/08/2025
+#Month of august practice.
+#1/08/2025
+
+#(a+b)*(a-b) =a^2 -b^2
+
+#a,b = 2,6
+#LHS=(2+6)+(2-6)
+#RHS= 2*2 -6*2
+#LHS=RHS
+#print()
+#(a+b)^3 = a^3 +b^3 +3ab(a+b)
+#a,b =2,9
+#Caluculate the compund intesrst
+
+#psthogras theorem
+#a2  = b2 +c2
+
+#Python Data Types
+
+#multiple code comment """ """
+"""
+"""1.number 2. Sequential 3. dict 4. set 5. Boolean
+1.number
+  int , float, complext number
+2. Sequential
+string , List,tuple
+
+3. Dict 
+
+4.set
+5. boolean  
+"""
+
+"""
+Prop of int
+Int data type is Immutable data type
+There is no linit for interger adata type
+whole number , both +ve and -ve also
+
+"""
+
+var1= 20
+var2= 123424
+var3=57689076534567897655456
+var4=4532
+print (var1,type(var1))
+print (var2,type(var2))
+print (var3,type(var3))
+print (var4,type(var4))
+
+print("_" *60)
+
+#Float
+
+"""
+imuutable ,no limit for float data type,. both +ve and -ne
+"""
+
+f1= 1.09
+f2= 337.9
+f3=99.7
+f4=99976.9
+print (f1,type(f1))
+print (f2,type(f2))
+print("+" *50)
+
+
+"""Complex data type
+immutable (change pnna mudiyathu )real and imaginary value combi of
+X+yj
+"""
+v1=10+10j
+print (v1,type(v1))
+print("real:",v1.real)
+print("img:",v1.imag)
+
+#add complex value
+
+#add_bjhjh = P1 +P2  y this is not workinh
+#print(add_bjhjh)
 
 
 
@@ -157,3 +234,67 @@ print(a,type[a])
 
 
 
+
+
+
+
+
+#4/08/2025
+
+
+#5/08/2025 Dictionary
+
+
+
+#6/08/2025 set
+
+"""set ={1,3,5,7,9,12,78}
+set.add(21)
+set.add(20)
+set.add(21)
+set.add(21)
+set.remove(3)
+print("-"*50)
+
+set1={1,8,6,8+50j,'python',(2,5,7), }
+print("value:",set)
+
+
+#rint(set, type(set))
+set.add(20)
+
+#oolean-bool
+
+a=5
+b=9
+c=7
+print(a==b)
+
+#conversion type conversion
+# n1=50 int>float
+
+#print(f1,type(f1))
+#nt>string
+
+#int>list its not iterable
+
+#boolean
+
+a = 9
+b = bool(a)
+print(b,type(a))
+
+a = 0 #null its false
+b = bool(a)
+print(b,type(a))
+
+
+a = {} #empty its false
+b = bool(a)
+print(b,type(a))
+
+#float > boolean
+f=0.8
+
+b= bool()
+"""
