a1570f4fd84504ac74638b0986dd2d7c5f112e34
Deepesh <xovek11875@bitvoo.com>
2025-08-13 20:54:23 +0530

    python if condition session2

---
+++ Deepesh/PythonProgramming/PythonIfCondition/class_work.py
@@ -0,0 +1,9 @@
+# Q1.  Write a python program to print square of the number if it is divisible by 3 or 7 else print cube
+# Q2.  Calculate the electricity bill as per the unit consumed.
+# get number of unit from user as input.
+# if unit > 100 :  per unit charge is 15
+# elif unit < 100 and unit > 200 :  per unit charge is 25
+# elif unit > 200 :  per unit charge is 30
+# else condition
+
+
