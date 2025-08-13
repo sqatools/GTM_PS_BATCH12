d779973d2786586245f306946dac48e8362f56ba
dharmanandab <dharmanandanet@gmail.com>
2025-08-13 21:03:25 +0530

    Adding first file to git from pycharm

---
+++ Dharmanand/python_if.py
@@ -0,0 +1,6 @@
+v1 = 3
+if v1 % 3:
+    print("square of the number is ", v1 ** 2)
+else:
+    if v1 % 7:
+        print("cube of the number is ", v1 ** 3)
--- Dharmanand/test1.py
+++ Dharmanand/test1.py
@@ -72,11 +72,14 @@ Area = 3.14 * r*r
 print ("Area of the square :", n**2)
 
 print ("Area of circle = 3.14 * r*r", Area)
+print ("Area", type(Area))
 n = [0,1,2]
 n.append(5)
 print ("value of n:", n)
 k=11
 print (k,id(k))
+
+print ('''serail''')
 
 
 
@@ -91,3 +94,79 @@ a=b=c=10
 print("values of a:",a)
 print("values of b:",b)
 print("values of c:",c)
+
+n_1 = 5
+
+print(n_1)
+
+Name = 'Rahul'
+NAme = 'John'
+
+print (Name,"|", NAme, "|")
+Div = 15%2
+print (Div)
+
+a = 4
+
+b = 4
+
+print (a!= b)
+
+print ("sqaure of a :", a**2)
+
+print ("cube of a :", a**3)
+
+
+
+a= 10 +20j
+print ("real value :", a.real)
+print ("imaginary value :", a.imag)
+print ('a',type(a))
+
+s = "image"
+print(s, type(s))
+
+print('''serail''')
+list = [1, "hello", {1,2,3}]
+
+print (list)
+
+list.append('hi')
+print (list, type(list))
+
+print (list[2])
+
+tuple = ('hi', "hi", 'nm')
+print (tuple[0])
+
+
+dict = {'name': 'Rahul', 'Age': 25, 'address':'asdf'}
+
+dict['name'] = 'Rupesh1'
+
+
+print (dict['name'])
+print ('Age :', dict['Age'])
+dict ['country'] ="India"
+print (dict['Age'])
+
+batch = {
+    'st1':{'name': 'Rahul', 'Age': 25, 'address':'asdf'},
+    'st2':{'name': 'Rahul1', 'Age': 26, 'address':'asdf1'}
+        }
+
+set = {'a','b','c','d'}
+set.add('a')
+
+set = {2.0,3,True, 'abc' }
+print (set, type(set))
+print (set)
+a = 5
+b = float (a)
+print (b, type(b))
+
+
+
+
+
+
