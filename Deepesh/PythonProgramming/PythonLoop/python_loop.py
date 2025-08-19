b7d166190d0136b7cdb7edf3e986579cd41b6267
Deepesh <xovek11875@bitvoo.com>
2025-08-19 20:07:54 +0530

    Python Loop session1

--- Deepesh/PythonProgramming/PythonLoop/python_loop.py
+++ Deepesh/PythonProgramming/PythonLoop/python_loop.py
@@ -67,9 +67,9 @@ for k in range(15):
 
 print("_" * 50)
 # write a python program add all value from 1 to 10
-import pdb;
-
-pdb.set_trace()
+# import pdb;
+#
+# pdb.set_trace()
 add = 0
 for val in range(1, 11, 1):
     add = add + val
@@ -77,6 +77,60 @@ for val in range(1, 11, 1):
 print("sum of 1 to 10:", add)
 # sum of 1 to 10: 55
 
+print("_"*40)
+############################################
 # home work:
 # 1. write a code add all even number from 1 to 20
 # 2. write a code to multiply value from 1 to 15
+
+for i in range(2, 20, 2):
+    print(i, end="")
+
+# 2 4 6 8 10 12 14 16 18
+
+print("_"*40)
+# 2. write a code to multiply value from 1 to 15
+multiply = 1
+for i in range(1, 15):
+    print(multiply, "*", i, "=", multiply*i)
+    multiply = multiply*i
+
+print("_"*50)
+##########################################
+# Use if condition inside for loop
+
+for i in range(1, 100):
+    if i%7 == 0:
+        print(i)
+
+print("_"*40)
+##################################
+# program to tag each value as odd or even from 1 to 20
+for j in range(1, 20):
+    if j%2 ==0:
+        print(j, ": even")
+    else:
+        print(j ,": odd")
+
+
+
+print("_"*50)
+##############################################
+# write a program to check given number is prime or not
+num = 7
+prime = True
+for i in range(2, num): # 2
+    if num%i == 0: # 7%2 == 0 | 7%3 == 0 | 7%4 == 0 | 7%5 == 0 | 7%6 == 0
+        prime = False
+
+if prime:
+    print("This is prime number :", num)
+else:
+    print("This is not prime number :", num)
+
+
+print("_"*40)
+########################################################
+# Nested for loop
+
+
