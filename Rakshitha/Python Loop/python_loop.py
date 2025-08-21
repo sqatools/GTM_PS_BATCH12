015db1ee323173eabbfe9119b2427d4a12f38712
rakshithas2003 <rakshirakshithas74@gmail.com>
2025-08-21 21:01:48 +0530

    if condition

--- Rakshitha/Python Loop/python_loop.py
+++ Rakshitha/Python Loop/python_loop.py
@@ -137,7 +137,8 @@ for i in range(1,6):
     print("Address :i",i)
     for i in range(1,4):
         print("package:j",j)
-print("_"*50)
+
+    print("_"*50)
 """
 Address :i 1
 package:j 9
@@ -158,4 +159,131 @@ package:j 9
 Address :i 5
 package:j 9
 package:j 9
+"""
+for i in range(1,5):
+    print("Address:i:",i)
+    for j in range(1,3):
+        print("Package:j:",j)
+
+    print("_"*50)
+"""
+ Address:i: 1
+Package:j: 1
+Package:j: 2
+__________________________________________________
+Address:i: 2
+Package:j: 1
+Package:j: 2
+__________________________________________________
+Address:i: 3
+Package:j: 1
+Package:j: 2
+__________________________________________________
+Address:i: 4
+Package:j: 1
+Package:j: 2
+"""
+###### continue and break statement
+# continue statement:
+for i in range(1,10):
+    if i in [2,5,7]:
+        continue
+    print(i,end=" ")
+#1 3 4 6 8 9
+print("_"*50)
+for i in range(1,10):
+    if i==5 :
+        continue
+    print(i,end=" ")
+# 1 2 3 4 6 7 8 9
+print("_"*50)
+for i in range(1,10):
+    if i==5 or i==7 or i==9:
+        continue
+    print(i,end=" ")
+# 1 2 3 4 6 8
+print("_"*50)
+
+######### Break #####
+#Break statement
+for i in range(1,10):
+    if i==5:
+        break
+    print(i,end=" ")
+#1 2 3 4
+print("_"*50)
+#Write the python program to get list of all the number b/w 1 to 100
+for num in range(1,100):
+    prime=True
+    for j in range(2,num):
+        if num%j==0:
+            prime=False
+            break
+
+    if prime:
+        print(num,end=" ")
+    else:
+        continue
+# 1 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97
+print("_"*50)
+#Write a python program to check two string are anagram to each other
+S1="python"
+S2="yohtnp"
+anagram=True
+for char in S1:
+    if char in S2:
+        continue
+    else:
+        anagram=False
+if anagram==True and len(S1)==len(S2):
+    print("these string are anagram:",S1,S2)
+else:
+    print("there string are not anagram:",S1,S2)
+#these string are anagram: python yohtnp
+s1="python"
+s2="yptnoha"
+anagram=True
+for char in s1:
+    if char in s2:
+        continue
+    else:
+        anagram=False
+if anagram==True and len(s1)==len(s2):
+    print("there string are anagram:",s1,s2)
+else:
+    print("there string are not anagram:",s1,s2)
+#there string are not anagram: python yptnoha
+s1 = "apython"
+s2 = "yptnoh"
+anagram = True
+for char in s1:
+    print(char)
+    if char in s2:
+        continue
+    else:
+        anagram = False
+        break
+# a
+# if anagram == True and len(s1) == len(s2):
+#     print("there string are anagram:", s1, s2)
+# else:
+#     print("there string are not anagram:", s1, s2)
+print("_"*50)
+s1 = "apython"
+s2 = "yptnoh"
+anagram = True
+for char in s1:
+    print(char)
+    if char in s2:
+        continue
+    else:
+        anagram = False
+"""
+a
+p
+y
+t
+h
+o
+n
 """
