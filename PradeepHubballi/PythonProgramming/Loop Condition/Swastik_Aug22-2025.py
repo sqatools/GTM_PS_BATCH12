805aac4130ec9841b6f5c422e702b6fc2b4b3457
Pradeep <pradeep.h147@gmail.com>
2025-08-22 18:37:08 +0530

    Adding first file to git from pycharm

--- PradeepHubballi/PythonProgramming/Loop Condition/Swastik_Aug22-2025.py
+++ PradeepHubballi/PythonProgramming/Loop Condition/Swastik_Aug22-2025.py
@@ -48,22 +48,22 @@ for i in range(1, 11):
             else:
                     print(pt2, end=" ")
         elif i == 7:
-            if j in [9,10,11,16,17,18]:
+            if j in [9,10,11,15,16,17,18]:
                 print(pt1, end=" ")
             else:
                 print(pt2, end=" ")
         elif i == 8:
-            if j in [9,10,11,16,17,18]:
+            if j in [9,10,11,15,16,17,18]:
                 print(pt1, end=" ")
             else:
                 print(pt2, end=" ")
         elif i == 9:
-            if j in [1,2,3,4,5,6,7,8,9,10,11,16,17,18]:
+            if j in [1,2,3,4,5,6,7,8,9,10,11,15,16,17,18]:
                 print(pt1, end=" ")
             else:
                 print(pt2, end=" ")
         elif i == 10:
-            if j in [1,2,3,4,5,6,7,8,9,10,11,16,17,18]:
+            if j in [1,2,3,4,5,6,7,8,9,10,11,15,16,17,18]:
                 print(pt1, end=" ")
             else:
                 print(pt2, end=" ")
