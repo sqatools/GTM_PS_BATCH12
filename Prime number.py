05d8f156fe2a45ea3fef08f283272c15d8810417
dharmanandab <dharmanandanet@gmail.com>
2025-08-25 12:10:44 +0530

    Added class work

--- Prime number.py
+++ Prime number.py
@@ -2,12 +2,30 @@
 
 for num in range(1,100):
     prime = True
-    for j in range(2, num):
-        if num%j == 0:
+    for j in range(2,num):
+        if num%j ==0:
             prime = False
             break
 
     if prime:
-        print(num)
+        print(num, end="")
     else:
         continue
+
+######################
+# write a python program to check two strings are anagram to each other:
+
+s1 = "python"
+s2 = "ythonp"
+anagram = True
+
+for char in s1:
+    if char in s2:
+        continue
+    else:
+       anagram = False
+    break
+
+if anagram ==True and len(s1) == len(s2):
+    print("These strings are anagram")
+
