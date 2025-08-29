90b91b07ba98a9f33037c912541734a9ae0f910b
sourabh.savadatti <savadatti.sourabh0@gmail.com>
2025-08-29 15:10:37 +0530

    function

--- sourabh/function/primenumber.py
+++ sourabh/function/primenumber.py
@@ -2,11 +2,11 @@ from typing import is_protocol
 
 
 def get_primenumber(num):
-    for i in range(2, num+1):
+    for i in range(2, num + 1):
         is_prime = True
-        for j in range()
-        if i % 1 == 0 and i % 2 == 0:
-            print(i)
+        for j in range(100, 0):
+            if i % 1 == 0 and i % 2 == 0:
+                print(i)
 
 
 get_primenumber(100)
