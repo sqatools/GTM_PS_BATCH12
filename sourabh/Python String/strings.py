cc079c6e493e84c623db9126ba25bde10533defa
sourabh.savadatti <savadatti.sourabh0@gmail.com>
2025-09-01 15:53:47 +0530

    function

---
+++ sourabh/Python String/strings.py
@@ -0,0 +1,10 @@
+s3 = "Hello, good morning, how are you doing"
+vowels = "aeiou"
+vowels_count = 0
+for char in s3:
+    if char in vowels:
+        vowels_count = vowels_count + 1
+    else:
+        continue
+
+print("all vowels:", vowels_count)
