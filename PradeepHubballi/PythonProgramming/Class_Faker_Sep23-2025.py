b12f3c0e1d015fe38084796bbbd855cf01819bab
Pradeep <pradeep.h147@gmail.com>
2025-09-24 20:05:23 +0530

    Faker to generate fake data

--- PradeepHubballi/PythonProgramming/Class_Faker_Sep23-2025.py
+++ PradeepHubballi/PythonProgramming/Class_Faker_Sep23-2025.py
@@ -9,5 +9,9 @@ for i in range (1,10) :
     print(i)
     print("first name :",fk.first_name())
     print("Last_name :",fk.last_name())
+    with open("user_data.txt", "a+") as file:
+        data = f"{fk.first_name()}, {fk.last_name()}, {fk.email()}, {fk.phone_number()}, {fk.credit_card_number()}\n"
+        file.write(data)
+    print("_"*50)
 
-
+print(dir(fk))
