78bb6e4a2dd8f9e83ff9dc74f1f2b5f532523255
sourabh.savadatti <savadatti.sourabh0@gmail.com>
2025-10-01 19:06:26 +0530

    selenium

---
+++ Deepesh/SeleniumCode/Basic/launch multiple browser.py
@@ -0,0 +1,22 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+Browser = "edge"
+
+driver = None
+
+if Browser.lower() == "chrome":
+    driver = webdriver.Chrome()
+elif Browser.lower() == "firefox":
+    driver = webdriver.Firefox()
+elif Browser.lower() == "edge":
+    driver = webdriver.Edge()
+
+driver.maximize_window()
+driver.implicitly_wait(10)
+
+driver.get("https://www.facebook.com")
+
+time.sleep(5)
